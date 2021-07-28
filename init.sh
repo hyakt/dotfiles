5b59da4802f0dcda19743feeaa40781213823788
hyakt <hyakt0@gmail.com>
2021-07-29 00:07:33 +0900

    update: init.sh

--- init.sh
+++ init.sh
@@ -6,10 +6,17 @@
 # https://brew.sh/index_ja
 which -s brew
 if [[ $? != 0 ]] ; then
+    echo "🐷 < Install homebrew!"
     curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 fi
 
-## create symlink
+# brew bundle install
+echo ""
+echo "🐷 < Bundle install homebrew!"
+brew bundle install
+
+echo ""
+echo "🐷 < Create dotfiles symlink!"
 mkdir -p ~/repos/github.com/hyakt/
 cd ~/repos/github.com/hyakt/
 git clone https://github.com/hyakt/dotfiles
@@ -18,36 +25,21 @@ chmod +x ./symlink.sh
 rm -rf ~/.config
 ./symlink.sh
 
-## fish
-brew install fish
-sudo sh -c "echo /usr/local/bin/fish >> /etc/shells"
-chsh -s /usr/local/bin/fish
-fish
-cd $__fish_config_dir
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-git reset --hard head
-fisher update
-
 ## tmux
-brew install tmux
+echo "🐷 < tpm!"
 mkdir -p ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-## installs
-brew install asdf
-brew install coreutils
-brew install curl
-brew install aspell
-brew install bash-completion
-brew install exa
-brew install fd
-brew install fzf
-brew install gh
-brew install ghq
-brew install git
-brew install htop
-brew install jq
-brew install peco
-brew install ripgrep
+## fish
+echo '🐷 < chsh fish!'
+if [[ $SHELL != "/usr/local/bin/fish" ]] ; then
+    sudo sh -c "echo /usr/local/bin/fish >> /etc/shells"
+    chsh -s /usr/local/bin/fish
+    fish
+fi
+# cd $__fish_config_dir
+# fish -c curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+# git reset --hard head
+# fisher update
 
 exit 0
