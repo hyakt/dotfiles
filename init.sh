7ba4fd44a7899ad8abab6197598b189667c83624
Hayato Kajiyama <me@piginbeer.com>
2023-06-16 15:42:20 +0900

    update: init script

--- init.sh
+++ init.sh
@@ -1,66 +1,44 @@
 #!/usr/bin/env bash
 
-REPO_PATH="$HOME/repos/github.com/hyakt/"
-TPM_PATH="$HOME/.config/tmux/plugins/tpm"
-
-
 # for macOS
 # -------------------
+DOTFILES_REPO_PATH="$HOME/repos/github.com/hyakt/dotfiles"
+TPM_PATH="$HOME/.config/tmux/plugins/tpm"
 
-echo ""
-echo "🐷< Install XCode Command Line"
+echo "🐷< Start setup! Maybe You'll fail, Sorry!"
 echo "--------------------"
 
-which -s xcode-select
-if [[ $? != 0 ]] ; then
-    xcode-select --install
-    else
-      echo "Already XCode Command Line installed";
-fi
-
-echo ""
-echo "🐷< Change shell to bash"
-echo "--------------------"
 chsh -s /bin/bash
 
-echo ""
-echo "🐷< Create dotfile symlinks"
-echo "--------------------"
-if [ ! -d $REPO_PATH/dotfiles ]; then
-    mkdir -p $REPO_PATH
-    git clone https://github.com/hyakt/dotfiles $REPO_PATH/dotfiles
+if test ! $(which xcode-select); then
+    echo "- install xcode"
+    xcode-select --install
 fi
-cd $REPO_PATH/dotfiles
+
+echo "- dotfile symlink"
+if [ ! -d $DOTFILES_REPO_PATH ]; then
+    mkdir -p $DOTFILES_REPO_PATH
+    git clone https://github.com/hyakt/dotfiles $DOTFILES_REPO_PATH
+fi
+cd $DOTFILES_REPO_PATH
 rm -rf ~/.config
 chmod +x ./symlink.sh
-sh ./symlink.sh
+bash -c ./symlink.sh
 
-echo ""
-echo "🐷< Install homebrew"
-echo "--------------------"
-which -s brew
-if [[ $? != 0 ]] ; then
+echo "- install homebrew"
+if test ! $(which brew); then
     curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
-    else
-      echo "Already homebrew installed";
 fi
 brew bundle --no-lock install
 
-echo ""
-echo "🐷< Setup tmux"
-echo "--------------------"
+echo "- setup tmux"
 if [ ! -d $TPM_PATH ]; then
     mkdir -p $TPM_PATH
     git clone https://github.com/tmux-plugins/tpm $TPM_PATH
 fi
-sh $TPM_PATH/scripts/install_plugins.sh
+bash -c $TPM_PATH/scripts/install_plugins.sh
 
-echo ""
-echo "🐷< Setup fish"
-echo "--------------------"
-fish -c "cd $__fish_config_dir;
-     curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher;
-     git checkout fish_plugins;
-     fisher update;"
+echo "- setup fish"
+fish -c 'cd $__fish_config_dir; curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher &&  git checkout fish_plugins && fisher update'
 
 exit 0
